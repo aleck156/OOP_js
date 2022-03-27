0395b06a65b304c13fc9888a7bc134999b493ecb
Aleksander Jaworski <aleck156@gmail.com>
2022-03-27 10:08:29 +0200

    add notes on foundation concepts of OOP

---
+++ lectures.js
@@ -0,0 +1,48 @@
+/*
+
+OOP - object oriented programming
+paradigm
+concept of object
+
+we use objects to model the real-world or abstract features
+
+object may contain data (properties) and code (methods)
+packing  data and the corresponding behavior into one block
+
+objects are self-contained pieces of code
+
+objects are building blocks of application, and interact with one another
+
+interactions happen through a public API - methods that the code outside of the object can access and use to cummunicate with the object
+
+OOP was developed with the goal of organizing code, to make it more flexible and easier to maintain
+    // avoiding spaghetti code is always a good practice
+
+
+// 4 MAIN CONCEPTS OF OJBECT-ORIENTED PROGRAMMING
+
+// ABSTRACTION
++ hide the low-level details tht don't matter to the end-user
++ contract - changes to the inside code cannot change how the end-user already interacts with API
+
+// ENCAPSULATION
++ keep some properties and methods private, inside the class, so they can't be accesses from the outside of the class
++ some methods can be exposed as a public API
++ the goal is to prevent external code to manipulate states without any control
+    + bugs
+    + security
+    + performance
+    + functionality
+    + code maintenance and expanding
++ leave only essential methods as public 
+
+// INHERITANCE
++ avoid code duplication
++ make all properties and methods of a certain class available to a child class
++ form a hierarchical relationship between classes
++ reuse common logic and model real-world relationships
+
+// POLYMORPHISM
++ a child class can overwrite a method it inherited from a parent class
+
+*/
