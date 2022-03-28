5d2c90802a41171d2b60b79739312acdbf9be484
Aleksander Jaworski <aleck156@gmail.com>
2022-03-28 10:21:26 +0200

    implement solution to challegne #1

--- challenge1.js
+++ challenge1.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed; // in km/h
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`Accelerating ... new speed: ${this.speed}`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`Braking hard! Speed dropped down to: ${this.speed}`);
+};
+
+const myCar = new Car('Ford', 210);
+console.log(myCar.make, myCar.speed);
+
+myCar.accelerate();
+console.log(myCar.make, myCar.speed);
+
+myCar.brake();
+myCar.brake();
+myCar.brake();
+myCar.brake();
+myCar.brake();
+myCar.brake();
+console.log(myCar.make, myCar.speed);
+
+const myCar1 = new Car('BMW', 120);
+const myCar2 = new Car('Mercedes', 95);
