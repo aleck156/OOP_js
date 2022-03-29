2b39e7f5a38a9a63d0fd9b59e60527ebc4280b95
Aleksander Jaworski <aleck156@gmail.com>
2022-03-29 08:46:21 +0200

    complete challenge #2 - js class

---
+++ challenge2.js
@@ -0,0 +1,39 @@
+'use strict';
+
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`[${this.make}] Accelerating. Current speed: ${this.speed}`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`[${this.make}] Accelerating. Current speed: ${this.speed}`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(_) {
+    this.speedUS = this.speed * 1.6;
+  }
+}
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+console.log(car2.speedUS);
+
+const car3 = new Car('ford', 120);
