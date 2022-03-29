400236c9ec467988d8c8f6bfd0c133e6346e764a
Aleksander Jaworski <aleck156@gmail.com>
2022-03-29 09:18:40 +0200

    implement proper inheritance

--- script2.js
+++ script2.js
@@ -24,6 +24,10 @@ const Student = function (firstName, birthYear, course) {
   this.course = course;
 };
 
+// with this line, Student class inherits all of methods available to Person class
+// it has to be added BEFORE any other methods are added to the Student.prototype
+Student.prototype = Object.create(Person.prototype);
+
 Student.prototype.introduce = function () {
   console.log(
     `Hi, my name is ${this.firstName}, I was born in ${this.birthYear}, and I study ${this.course}`
@@ -34,3 +38,7 @@ const mike = new Student('Mike', 2020, 'Computer Science');
 console.log(mike);
 
 mike.introduce();
+
+// calling methods from parent class does not work ... yet
+// but once we connect Student.prototype with Person.prototype with Object.create, it works!
+mike.calcAge();
