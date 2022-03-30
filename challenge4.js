9eede22946e26b7ff066b615d1ef6bdbcc144da9
Aleksander Jaworski <aleck156@gmail.com>
2022-03-30 10:49:07 +0200

    implement solution to challenge 4

--- challenge4.js
+++ challenge4.js
@@ -3,6 +3,12 @@ class CarCl {
     this.make = make;
     this.speed = speed;
   }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} braking to ${this.speed} km/h.`);
+    return this;
+  }
 }
 
 class EVCl extends CarCl {
@@ -12,4 +18,41 @@ class EVCl extends CarCl {
     super(make, speed);
     this.charge = charge;
   }
+
+  chargeBattery(chargeTo) {
+    this.charge = chargeTo;
+    console.log(`${this.make} charged battery to ${this.charge}`);
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.charge--;
+    console.log(
+      `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+    );
+    return this;
+  }
 }
+
+const car1 = new EVCl('Hyundai', 110, 74);
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.chargeBattery(95);
+car1.brake();
+
+const car2 = new EVCl('Tesla', 75, 34);
+car2
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(99)
+  .accelerate()
+  .brake();
